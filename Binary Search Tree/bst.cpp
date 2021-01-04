3f9fafffefda28ed0d26043a5f01476c28fc8f43
iambharatks <iambharatks@gmail.com>
2021-01-04 18:01:52 +0530

    Daily Problem Solving

---
+++ Binary Search Tree/bst.cpp
@@ -0,0 +1,136 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node(int data, Node *left = NULL, Node *right = NULL) : data(data), left(left), right(right) {}
+};
+Node *search(Node **root, int key)
+{
+    if (!(*root))
+        return (*root) = new Node(key);
+    if ((*root)->data == key)
+        return NULL;
+    if ((*root)->data > key)
+    {
+        if ((*root)->left)
+            return search(&(*root)->left, key);
+        (*root)->left = new Node(key);
+        return (*root)->left;
+    }
+    if ((*root)->right)
+        return search(&(*root)->right, key);
+    (*root)->right = new Node(key);
+    return (*root)->right;
+}
+
+class bst
+{
+    int nodes;
+
+public:
+    Node *root;
+    bst(Node *root = NULL) : root(root), nodes(0) {}
+    int number_of_nodes() { return nodes; }
+
+    void insert(int data)
+    {
+        if (search(&root, data))
+            nodes++;
+        return;
+    }
+    void lot()
+    {
+        if (!root)
+            return;
+        Node *tmp;
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            tmp = q.front();
+            cout << tmp->data << " ";
+            q.pop();
+            if (tmp->left)
+                q.push(tmp->left);
+            if (tmp->right)
+                q.push(tmp->right);
+        }
+        cout << endl;
+    }
+    int min()
+    {
+        if (!root)
+            return INT_MAX;
+        Node *current = root;
+        while (current && current->left)
+            current = current->left;
+        return current->data;
+    }
+    int max()
+    {
+        if (!root)
+            return INT_MIN;
+        Node *current = root;
+        while (current && current->right)
+            current = current->right;
+        return current->data;
+    }
+};
+Node *minValueNode(Node *root)
+{
+    Node *current = root;
+    while (current && current->left)
+        current = current->left;
+    return current;
+}
+Node *deleteNode(Node *root, int key)
+{
+    if (root == NULL)
+        return NULL;
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+    else
+    {
+        if (root->left == NULL)
+        {
+            Node *tmp = root->right;
+            free(root);
+            return tmp;
+        }
+        else if (root->right == NULL)
+        {
+            Node *tmp = root->left;
+            free(root);
+            return tmp;
+        }
+        Node *tmp = minValueNode(root->right);
+        root->data = tmp->data;
+        root->right = deleteNode(root->right, tmp->data);
+    }
+    return root;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    bst tree;
+    while (n--)
+    {
+        int data;
+        cin >> data;
+        tree.insert(data);
+    }
+    cout << tree.number_of_nodes() << "\n";
+    tree.lot();
+    tree.root = deleteNode(tree.root, 1);
+    tree.lot();
+}
