44cb56e9def88fac3b72b543956939e4e9ac467b
iambharatks <iambharatks@gmail.com>
2021-01-27 23:19:55 +0530

    RotateBy90

--- Matrix/rotate90.cpp
+++ Matrix/rotate90.cpp
@@ -5,9 +5,32 @@ using namespace std;
 
 void rotate90(vector<vector<int>> &matrix, int r, int c)
 {
-    
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            if (i > j)
+                swap(matrix[i][j], matrix[j][i]);
+    // for (int i = 0; i < r; i++)
+    // {
+    //     for (int j = 0; j < c / 2; j++)
+    //         swap(matrix[i][j], matrix[i][c - j - 1]);
+    // }
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+            cout << matrix[i][j] << " ";
+        cout << "\n";
+    }
 }
-
+void rotateby90Anti(vector<vector<int>> &matrix, int n)
+{
+    // code here
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (i > j)
+                swap(matrix[i][j], matrix[j][i]);
+    for (int i = 0; i < n / 2; i++)
+        swap(matrix[i], matrix[n - i - 1]);
+}
 
 int main()
 {
@@ -19,5 +42,11 @@ int main()
         for (int &i : v)
             cin >> i;
     }
-    rotate90(matrix, r, c);
+    rotateby90Anti(matrix, r);
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+            cout << matrix[i][j] << " ";
+        cout << "\n";
+    }
 }
