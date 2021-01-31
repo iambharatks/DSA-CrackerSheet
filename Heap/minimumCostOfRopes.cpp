91bd60859b62f99bbf80c8edbb3296f21d27bf71
iambharatks <iambharatks@gmail.com>
2021-01-31 14:34:50 +0530

    Priority Queue

---
+++ Heap/minimumCostOfRopes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Minimum Cost of ropes
+//using minHeap
+long long minCost(long long arr[], long long n)
+{
+    
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    long long arr[n];
+    for (long long &i : arr)
+        cin >> i;
+}
